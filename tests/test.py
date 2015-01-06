f7374c1474c2df646473926de1d016e762e78b3a
Chris Terman <cjt@mit.edu>
2015-01-05 19:50:15 -0500

    lab 3 stuff

--- tests/test.py
+++ tests/test.py
@@ -60,7 +60,7 @@ def cmp_test_cycle(f,fn,z,v,n,y):
     field(f,2,fn,'01')
     field(f,3,(z << 2)+(v << 1)+n,'01')
     field(f,32,y,'LH')
-    fn = ['CMPEQ','CMPLT','CMPLE'][fn]
+    fn = ['???','CMPEQ','CMPLT','CMPLE'][fn]
     f.write('// {:2d}: fn={:s}, z={:d}, v={:d}, n={:d}, y={:d}\n'.format(cycle,fn,z,v,n,y & 1))
 
 def cmp_test(f):
@@ -70,9 +70,9 @@ def cmp_test(f):
         z = (zvn >> 2) & 1
         v = (zvn >> 1) & 1
         n = zvn & 1
-        cmp_test_cycle(f,0b00,z,v,n,z)
-        cmp_test_cycle(f,0b01,z,v,n,n ^ v)
-        cmp_test_cycle(f,0b10,z,v,n,z | (n ^ v))
+        cmp_test_cycle(f,0b01,z,v,n,z)
+        cmp_test_cycle(f,0b10,z,v,n,n ^ v)
+        cmp_test_cycle(f,0b11,z,v,n,z | (n ^ v))
 
 #cmp_test(sys.stdout)
 
@@ -92,7 +92,7 @@ def arith_test_cycle(f,fn,a,b,y,z,v,n):
 
 def arith_result(fn,a,b):
     amsb = (a >> 31) & 1;
-    if fn == 0:  # add
+    if (fn & 1) == 0:  # add
         y = a + b
         bmsb = (b >> 31) & 1
     else:
@@ -146,17 +146,17 @@ def shift_test(f):
 ##################################################
 
 op = [
-    "ADD", "SUB", "MUL", "???", "???", "CMPEQ", "???", "CMPLT",
-    "SHL", "SHR", "???", "SRA", "???", "CMPLE", "???", "???",
-    "F0000", "F0001", "F0010", "F0011", "F0100", "F0101", "XOR", "F0111",
-    "AND", "XNOR", "A", "F1011", "F1100", "F1101", "OR", "F1111"
+    "?", "?", "?", "CMPEQ", "?", "CMPLT", "?", "CMPLE", "?", "?", "?", "?", "?", "?", "?", "?",
+    "ADD", "SUB", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?",
+    "F0000", "F0001", "F0010", "F0011", "F0100", "F0101", "XOR", "F0111", "AND", "XNOR", "A", "F1011", "F1100", "F1101", "OR", "F1111",
+    "SHL", "SHR", "?", "SRA", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?",
 ]
 
 def alu_test_cycle(f,fn,a,b,y):
     global cycle
     cycle += 1
     aluy,z,v,n = arith_result(fn & 1,a,b)
-    field(f,5,fn,'01')
+    field(f,6,fn,'01')
     field(f,32,a,'01')
     field(f,32,b,'01')
     field(f,32,y,'LH')
@@ -168,61 +168,70 @@ def alu_test(f):
     cycle = 0
 
     # test boole
+    BOOL = 0b100000
     a = 0xFF00FF00
     b = 0xFFFF0000
-    alu_test_cycle(f,0b10000,a,b,0)
-    alu_test_cycle(f,0b10001,a,b,~(a | b))
-    alu_test_cycle(f,0b10010,a,b,a & ~b)
-    alu_test_cycle(f,0b10011,a,b,~b)
-    alu_test_cycle(f,0b10100,a,b,~a & b)
-    alu_test_cycle(f,0b10101,a,b,~a)
-    alu_test_cycle(f,0b10110,a,b,a ^ b)
-    alu_test_cycle(f,0b10111,a,b,~(a & b))
-    alu_test_cycle(f,0b11000,a,b,a & b)
-    alu_test_cycle(f,0b11001,a,b,~(a ^ b))
-    alu_test_cycle(f,0b11010,a,b,a)
-    alu_test_cycle(f,0b11011,a,b,a | ~b)
-    alu_test_cycle(f,0b11100,a,b,b)
-    alu_test_cycle(f,0b11101,a,b,~a | b)
-    alu_test_cycle(f,0b11110,a,b,a | b)
-    alu_test_cycle(f,0b11111,a,b,-1)
+    alu_test_cycle(f,BOOL + 0b0000,a,b,0)
+    alu_test_cycle(f,BOOL + 0b0001,a,b,~(a | b))
+    alu_test_cycle(f,BOOL + 0b0010,a,b,a & ~b)
+    alu_test_cycle(f,BOOL + 0b0011,a,b,~b)
+    alu_test_cycle(f,BOOL + 0b0100,a,b,~a & b)
+    alu_test_cycle(f,BOOL + 0b0101,a,b,~a)
+    alu_test_cycle(f,BOOL + 0b0110,a,b,a ^ b)
+    alu_test_cycle(f,BOOL + 0b0111,a,b,~(a & b))
+    alu_test_cycle(f,BOOL + 0b1000,a,b,a & b)
+    alu_test_cycle(f,BOOL + 0b1001,a,b,~(a ^ b))
+    alu_test_cycle(f,BOOL + 0b1010,a,b,a)
+    alu_test_cycle(f,BOOL + 0b1011,a,b,a | ~b)
+    alu_test_cycle(f,BOOL + 0b1100,a,b,b)
+    alu_test_cycle(f,BOOL + 0b1101,a,b,~a | b)
+    alu_test_cycle(f,BOOL + 0b1110,a,b,a | b)
+    alu_test_cycle(f,BOOL + 0b1111,a,b,-1)
 
     # test shift
+    SHL = 0b110000
+    SHR = 0b110001
+    SRA = 0b110011
     for a in (0,1,0xFFFFFFFF,0x12345678,0xFEDCAB98):
         for b in (0,1,2,4,8,16,31):
-            alu_test_cycle(f,0b01000,a,b,a << b)
-            alu_test_cycle(f,0b01001,a,b,a >> b)
-            alu_test_cycle(f,0b01011,a,b,(a if a < 0x80000000 else 0xFFFFFFFF00000000+a) >> b)
+            alu_test_cycle(f,SHL,a,b,a << b)
+            alu_test_cycle(f,SHR,a,b,a >> b)
+            alu_test_cycle(f,SRA,a,b,(a if a < 0x80000000 else 0xFFFFFFFF00000000+a) >> b)
 
     # test arith
-    for fn in (0,1):
+    ADD = 0b010000
+    SUB = 0b010001
+    for fn in (ADD, SUB):
         for a in (0,1,-1,0xAAAAAAAA,0x55555555):
             for b in (0,1,-1,0xAAAAAAAA,0x55555555):
                 y,z,v,n = arith_result(fn,a,b)
                 alu_test_cycle(f,fn,a,b,y)
 
     # test cmp
-    alu_test_cycle(f,0b00101,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0  CMPEQ
-    alu_test_cycle(f,0b00111,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0  CMPLT
-    alu_test_cycle(f,0b01101,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0  CMPLE
+    CMPEQ = 0b000011
+    CMPLT = 0b000101
+    CMPLE = 0b000111
+    alu_test_cycle(f,CMPEQ,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0
+    alu_test_cycle(f,CMPLT,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0
+    alu_test_cycle(f,CMPLE,0x00000005,0xDEADBEEF,0) # z=0, v=0, n=0
 
-    alu_test_cycle(f,0b00101,0x12345678,0x12345678,1) # z=1, v=0, n=0  CMPEQ
-    alu_test_cycle(f,0b00111,0x12345678,0x12345678,0) # z=1, v=0, n=0  CMPLT
-    alu_test_cycle(f,0b01101,0x12345678,0x12345678,1) # z=1, v=0, n=0  CMPLE
+    alu_test_cycle(f,CMPEQ,0x12345678,0x12345678,1) # z=1, v=0, n=0
+    alu_test_cycle(f,CMPLT,0x12345678,0x12345678,0) # z=1, v=0, n=0
+    alu_test_cycle(f,CMPLE,0x12345678,0x12345678,1) # z=1, v=0, n=0
 
-    alu_test_cycle(f,0b00101,0x80000000,0x00000001,0) # z=0, v=1, n=0  CMPEQ
-    alu_test_cycle(f,0b00111,0x80000000,0x00000001,1) # z=0, v=1, n=0  CMPLT
-    alu_test_cycle(f,0b01101,0x80000000,0x00000001,1) # z=0, v=1, n=0  CMPLE
+    alu_test_cycle(f,CMPEQ,0x80000000,0x00000001,0) # z=0, v=1, n=0
+    alu_test_cycle(f,CMPLT,0x80000000,0x00000001,1) # z=0, v=1, n=0
+    alu_test_cycle(f,CMPLE,0x80000000,0x00000001,1) # z=0, v=1, n=0
 
-    alu_test_cycle(f,0b00101,0xDEADBEEF,0x00000005,0) # z=0, v=0, n=1  CMPEQ
-    alu_test_cycle(f,0b00111,0xDEADBEEF,0x00000005,1) # z=0, v=0, n=1  CMPLT
-    alu_test_cycle(f,0b01101,0xDEADBEEF,0x00000005,1) # z=0, v=0, n=1  CMPLE
+    alu_test_cycle(f,CMPEQ,0xDEADBEEF,0x00000005,0) # z=0, v=0, n=1
+    alu_test_cycle(f,CMPLT,0xDEADBEEF,0x00000005,1) # z=0, v=0, n=1
+    alu_test_cycle(f,CMPLE,0xDEADBEEF,0x00000005,1) # z=0, v=0, n=1
 
-    alu_test_cycle(f,0b00101,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1  CMPEQ
-    alu_test_cycle(f,0b00111,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1  CMPLT
-    alu_test_cycle(f,0b01101,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1  CMPLE
+    alu_test_cycle(f,CMPEQ,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1
+    alu_test_cycle(f,CMPLT,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1
+    alu_test_cycle(f,CMPLE,0x7FFFFFFF,0xFFFFFFFF,0) # z=0, v=1, n=1
 
-#alu_test(sys.stdout)
+alu_test(sys.stdout)
 
 ##################################################
 ##  regfile
